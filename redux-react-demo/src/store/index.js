ab0b272a7dfe43a8f96b10418e9b64605f8fbbc9
arundhati-shreya <arundhati0426@gmail.com>
2024-01-18 02:05:32 +0530

    Auth Reducer

--- redux-react-demo/src/store/index.js
+++ redux-react-demo/src/store/index.js
@@ -1,32 +1,11 @@
-import { createStore } from 'redux';
+import { configureStore } from '@reduxjs/toolkit';
 
-const counterReducer = (state = { counter: 0 }, action) => {
-  if (action.type === 'increment') {
-    return {
-      counter: state.counter + 2,
-    };
-  }
+import counterReducer from './counter';
+import authReducer from './auth';
 
-  if (action.type === 'decrement') {
-    return {
-      counter: state.counter - 2,
-    };
-  }
-  if (action.type === 'incrementBy') {
-    return {
-      counter: state.counter + action.value,
-    };
-  }
 
-  if (action.type === 'decrementBy') {
-    return {
-      counter: state.counter - action.value,
-    };
-  }
-
-  return state;
-};
-
-const store = createStore(counterReducer);
+const store = configureStore({
+  reducer: { counter: counterReducer, auth: authReducer },
+});
 
 export default store;
